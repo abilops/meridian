bd2178725784e7b33630314ecf6ef26469520d39
abilops <abilops@users.noreply.github.com>
2016-09-10 21:32:23 +0530

    Basic file reading framework

--- main.c
+++ main.c
@@ -1,1 +1,28 @@
-#merit-based allocation software for high-demand low-supply situations
+//merit-based allocation software for high-demand low-supply situations
+
+#include <stdio.h>
+#include <math.h>
+#include <strings.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		printf("Usage:\n./meridian infile outfile\n");
+	}
+	FILE* f = fopen(argv[1], "r");
+	if (f == NULL)
+	{
+		printf("Error opening file.\n");
+		fclose(f);
+		return -1;
+	}
+	char c;
+	for (c = fgetc(f); c != EOF; c = fgetc(f))
+	{
+		printf("%c",c);
+	}
+	fclose(f);
+	return 0;
+}
