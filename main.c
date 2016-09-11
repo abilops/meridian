f6daf659eeb8e2784d5b2ab490f1fdc00e8f05d2
abilops <abilops@users.noreply.github.com>
2016-09-11 16:48:12 +0530

    Reads students.dat in a very stupid way

--- main.c
+++ main.c
@@ -2,36 +2,90 @@
 
 #include <stdio.h>
 #include <math.h>
-#include <strings.h>
+#include <string.h>
 #include <stdlib.h>
 #include "structs.h"
 
 int main(int argc, char* argv[])
 {
-	if (argc != 2)
+	if (argc != 4)
 	{
-		printf("Usage:\n./meridian infile outfile\n");
-	}
-	FILE* f = fopen(argv[1], "r");
-	if (f == NULL)
-	{
-		printf("Error opening file.\n");
-		fclose(f);
+		printf("Usage:\n./meridian demands applicants results\n");
 		return -1;
 	}
-	char c;
-	for (c = fgetc(f); c != EOF; c = fgetc(f))
+	FILE* appf = fopen(argv[2], "r");
+	if (appf == NULL)
 	{
-		printf("%c",c);
+		printf("Error opening file.\n");
+		fclose(appf);
+		return -1;
 	}
-	fclose(f);
-	printf("Enter name for applicant1: ");
-	applicant a1;
-	a1.name = malloc(20);
-	scanf("%s", a1.name);
-	printf("Enter id for a1: ");
-	scanf("%lu", &a1.id);
-	printf("%s's id is %lu.\n", a1.name, a1.id);
-	free(a1.name);
+	// Find number of applicants
+	unsigned int appnum = 0;
+	for (char c = fgetc(appf); c != EOF; c = fgetc(appf))
+	{
+		if (c == '\n')
+		{
+			appnum++;
+		}
+	}
+
+	// Array of applicants
+	applicant people[appnum];
+
+	// Set pointer back to beginning
+	fseek(appf, 0, SEEK_SET);
+
+	char buffer[81];
+	int bindex = 0;
+	int column = 0;
+	int pindex = 0;
+	for (char c = fgetc(appf); c != EOF; c = fgetc(appf))
+	{
+		if (c == ',' || c == '\n')
+		{
+			buffer[bindex] = '\0';
+			switch (column)
+			{
+				case (0):
+					//this must be the marks
+					people[pindex].marks = atoi(buffer);
+					break;
+				case (1):
+					// this must be the id
+					people[pindex].id = atoi(buffer);
+					break;
+				case (2):
+					// this is the name
+					people[pindex].name = malloc(strlen(buffer) + 1);
+					strcat(people[pindex].name, &buffer[0]);
+					break;
+				case (3):
+					// this must be the preferences
+					people[pindex].prefs = malloc(strlen(buffer) + 1);
+					strcat(people[pindex].prefs, &buffer[0]);
+					break;
+			}
+			column++;
+			bindex = 0;
+			// if newline then 
+			if (c == '\n')
+			{
+				pindex++;
+				column = 0;
+			}
+		}
+		else
+		{
+			buffer[bindex] = c;
+			bindex++;
+		}
+	}
+	for (int i = 0; i < appnum; i++)
+	{
+		printf("%lu|%s\n%d|%s\n", people[i].id, people[i].name, people[i].marks, people[i].prefs);
+		free(people[i].name);
+		free(people[i].prefs);
+	}
 	return 0;
 }
--- structs.h
+++ structs.h
@@ -2,4 +2,11 @@ typedef struct applicant {
 	char* name;
 	unsigned long int id;
 	char* prefs;
+	unsigned int marks;
 } applicant;
+
+typedef struct demand {
+	unsigned long int id;
+	char* name;
+	unsigned int seats;
+} demand;
