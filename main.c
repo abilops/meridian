61ff736863e7165bad56c577cdecd75c3526ad90
abilops <abhure@outlook.com>
2016-09-13 10:47:28 +0530

    Function getnextcol added for reading CSVs and importing data

--- main.c
+++ main.c
@@ -6,6 +6,8 @@
 #include <stdlib.h>
 #include "structs.h"
 
+void getnextcol(FILE* file, char* plate);
+
 int main(int argc, char* argv[])
 {
 	if (argc != 4)
@@ -35,57 +37,46 @@ int main(int argc, char* argv[])
 
 	// Set pointer back to beginning
 	fseek(appf, 0, SEEK_SET);
-
-	char buffer[81];
-	int bindex = 0;
-	int column = 0;
-	int pindex = 0;
-	for (char c = fgetc(appf); c != EOF; c = fgetc(appf))
-	{
-		if (c == ',' || c == '\n')
-		{
-			buffer[bindex] = '\0';
-			switch (column)
-			{
-				case (0):
-					//this must be the marks
-					people[pindex].marks = atoi(buffer);
-					break;
-				case (1):
-					// this must be the id
-					people[pindex].id = atoi(buffer);
-					break;
-				case (2):
-					// this is the name
-					people[pindex].name = malloc(strlen(buffer) + 1);
-					strcat(people[pindex].name, &buffer[0]);
-					break;
-				case (3):
-					// this must be the preferences
-					people[pindex].prefs = malloc(strlen(buffer) + 1);
-					strcat(people[pindex].prefs, &buffer[0]);
-					break;
-			}
-			column++;
-			bindex = 0;
-			// if newline then 
-			if (c == '\n')
-			{
-				pindex++;
-				column = 0;
-			}
-		}
-		else
-		{
-			buffer[bindex] = c;
-			bindex++;
-		}
-	}
+	char* buffer = malloc(81);
 	for (int i = 0; i < appnum; i++)
 	{
-		printf("%lu|%s\n%d|%s\n", people[i].id, people[i].name, people[i].marks, people[i].prefs);
+		// Init person in context for easy typing
+		applicant* pic = &people[i];
+		getnextcol(appf, buffer);
+		pic->marks = atoi(buffer);
+		getnextcol(appf,buffer);
+		pic->id = atoi(buffer);
+		getnextcol(appf,buffer);
+		pic->name = malloc(strlen(buffer));
+		strcat(pic->name, buffer);
+		getnextcol(appf,buffer);
+		pic->prefs = malloc(strlen(buffer));
+		strcat(pic->prefs, buffer);
+	}
+	free(buffer);
+	fclose(appf);
+	
+	for (int i = 0; i < appnum; i++)
+	{
+		applicant* pic = &people[i];
+		printf("%lu|%s\n%d|%s\n", pic->id, pic->name, pic->marks, pic->prefs);
 		free(people[i].name);
 		free(people[i].prefs);
 	}
 	return 0;
+}
+
+// Serves the value of the next column in the CSV file in the
+// provided plate
+void getnextcol(FILE* file, char* plate)
+{
+	int pindex = 0;
+	char c = fgetc(file);
+	while (c != ',' && c != '\n' && c != EOF)
+	{
+		plate[pindex] = c;
+		pindex++;
+		c = fgetc(file);
+	}
+	plate[pindex] = '\0';
 }
