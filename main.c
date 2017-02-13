e613f05d738ae87e13750cea86ad3894d0d3fa40
abilops <abilops@users.noreply.github.com>
2017-02-13 10:42:06 +0530

    Functions and headers in separate files & shiny new Makefile!

--- main.c
+++ main.c
@@ -5,13 +5,12 @@
 #include <ctype.h>
 #include <string.h>
 #include <stdlib.h>
-#include "structs.h"
 #include <sys/stat.h>
 #include <sys/types.h>
 
 #define INT_SIZE 2
-void getnextcol(FILE* file, char* plate);
-void sort(applicant people[]);
+
+#include "meridian.h"
 
 int main(int argc, char* argv[])
 {
@@ -276,32 +275,7 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
-// Serves the value of the next column in the CSV file in the
-// provided plate
-void getnextcol(FILE* file, char* plate)
-{
-	int pindex = 0;
-	char c = fgetc(file);
-	while (c != ',' && c != '\n' && c != EOF)
-	{
-		plate[pindex] = c;
-		pindex++;
-		c = fgetc(file);
-	}
-	plate[pindex] = '\0';
-}
-
 /* void getnextline(FILE* file, char* plate)
 {
 	// TODO
 } */
-// Supposed to sort the people according to MARKS only
-// but if you are importing a CSV, then why not use
-// some spreadsheet program to sort it before the export?
-void sort (applicant people[])
-{
-	// TODO
-	printf("Sorting not yet implemented.\nAssuming that input is already sorted.");
-	return;
-}
-
